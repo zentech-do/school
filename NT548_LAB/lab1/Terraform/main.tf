935e68504e58348c022bb99ca072bbc72b83bd82
nguyenlocgh <asdloc098l@gmail.com>
2024-10-14 21:48:15 +0700

    update output

--- NT548_LAB/lab1/Terraform/main.tf
+++ NT548_LAB/lab1/Terraform/main.tf
@@ -13,3 +13,51 @@ module "vpc" {
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "public_instance_ids" {
+  value = module.vpc.public_instance_ids
+}
+
+output "private_instance_ids" {
+  value = module.vpc.private_instance_ids
+}
+
+output "public_instance_public_ips" {
+  value = module.vpc.public_instance_public_ips
+}
+
+output "private_instance_private_ips" {
+  value = module.vpc.private_instance_private_ips
+}
+
+output "public_security_group_id" {
+  value = module.vpc.public_security_group_id
+}
+
+output "private_security_group_id" {
+  value = module.vpc.private_security_group_id
+}
+
+output "internet_gateway_id" {
+  value = module.vpc.internet_gateway_id
+}
+
+output "nat_gateway_id" {
+  value = module.vpc.nat_gateway_id
+}
+
+output "public_route_table_id" {
+  value = module.vpc.public_route_table_id
+}
+
+output "private_route_table_id" {
+  value = module.vpc.private_route_table_id
+}
