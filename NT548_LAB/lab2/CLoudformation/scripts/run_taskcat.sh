b70d161ca352d86493eadbc98f3ab005373bd633
HongQuan1812 <lehongquan202@gmail.com>
2024-11-13 11:03:17 +0700

    everything is ok

---
+++ NT548_LAB/lab2/CLoudformation/scripts/run_taskcat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Extract values from env.json
+ProjectName=$(jq -r '.[] | select(.ParameterKey == "ProjectName") | .ParameterValue' CI-CD/env.json)
+
+# Use envsubst to replace the variables in .taskcat.template.yml
+export ProjectName
+
+envsubst < .taskcat.template.yml > .taskcat.yml
+
+# Run taskcat
+taskcat test run
---
+++ NT548_LAB/lab2/README.md
@@ -0,0 +1,16 @@
+- **Change directory:**
+  ```
+  cd NT548_LAB/lab2/CLoudformation
+  ```
+- **Create CodePipeline-stack, run the command:**
+
+  ```
+  ./scripts/create_stack.sh
+  ```
+
+  - Set parameters in `env.json`
+
+- **Delete stack, run the command:**
+  ```
+  ./scripts/delete_stack.sh
+  ```
